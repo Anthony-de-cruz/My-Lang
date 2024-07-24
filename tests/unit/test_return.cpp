0b4547eebc08ec76f397cd94d0a9aca0e99d7fb3
Anthony de Cruz <anthonydecruz@proton.me>
2024-07-24 14:23:32 +0100

    wrote basic stream test

--- tests/unit/test_return.cpp
+++ tests/unit/test_return.cpp
@@ -1,9 +1,23 @@
 #include <gtest/gtest.h>
+#include <sstream>
 
 #include "lexer.h"
 
 class LexerTest : public testing::Test {
   protected:
+    Lexer *lexer;
+
+    LexerTest() { lexer = new Lexer; };
 };
 
-TEST(LexerTest, BasicTest) { ASSERT_EQ(1, 1); }
+TEST_F(LexerTest, BasicTest) {
+
+    auto stream = new std::stringstream;
+
+    *stream << "test string";
+
+    ASSERT_EQ(lexer->get_token(stream), -4);
+    ASSERT_EQ(lexer->get_identifier(), "test");
+    ASSERT_EQ(lexer->get_token(stream), -4);
+    ASSERT_EQ(lexer->get_identifier(), "string");
+}
