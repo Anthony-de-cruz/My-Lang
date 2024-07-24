8694e24ff9ef3e9cc20f8879d9fb6e75b0793deb
Anthony de Cruz <anthonydecruz@proton.me>
2024-07-24 20:18:02 +0100

    added more tests

--- tests/unit/lexer_tokens.cpp
+++ tests/unit/lexer_tokens.cpp
@@ -15,9 +15,57 @@ class LexerTest : public testing::Test {
     };
 };
 
-TEST_F(LexerTest, StringOfTokens) {
+TEST_F(LexerTest, Eof) { ASSERT_EQ(lexer->get_token(stream), lexer->tok_eof); }
 
-    *stream << "test string 123.3 #comment" << EOF;
+TEST_F(LexerTest, Def) {
+    *stream << "def";
+
+    ASSERT_EQ(lexer->get_token(stream), lexer->tok_def);
+    ASSERT_EQ(lexer->get_identifier(), "def");
+}
+
+TEST_F(LexerTest, Extern) {
+    *stream << "extern";
+
+    ASSERT_EQ(lexer->get_token(stream), lexer->tok_extern);
+    ASSERT_EQ(lexer->get_identifier(), "extern");
+}
+
+TEST_F(LexerTest, Identifier) {
+    *stream << "test";
+
+    ASSERT_EQ(lexer->get_token(stream), lexer->tok_identifier);
+    ASSERT_EQ(lexer->get_identifier(), "test");
+}
+
+TEST_F(LexerTest, Integer) {
+    *stream << "123";
+
+    ASSERT_EQ(lexer->get_token(stream), lexer->tok_number);
+    ASSERT_EQ(lexer->get_numeric(), 123);
+}
+
+TEST_F(LexerTest, Decimal) {
+    *stream << "123.4";
+
+    ASSERT_EQ(lexer->get_token(stream), lexer->tok_number);
+    ASSERT_EQ(lexer->get_numeric(), 123.4);
+}
+
+TEST_F(LexerTest, Unknown) {
+    *stream << "+";
+
+    ASSERT_EQ(lexer->get_token(stream), 43);
+}
+
+TEST_F(LexerTest, Comment) {
+    *stream << "# wee hello hi";
+
+    ASSERT_EQ(lexer->get_token(stream), Lexer::tok_eof);
+}
+
+TEST_F(LexerTest, StringOfTokens) {
+    *stream << "test string 123.3 #comment";
 
     ASSERT_EQ(lexer->get_token(stream), Lexer::tok_identifier);
     ASSERT_EQ(lexer->get_identifier(), "test");
@@ -29,4 +77,20 @@ TEST_F(LexerTest, StringOfTokens) {
     ASSERT_EQ(lexer->get_numeric(), 123.3);
 
     ASSERT_EQ(lexer->get_token(stream), Lexer::tok_eof);
+}
+
+TEST_F(LexerTest, FunctionDef) {
+    *stream << "def my_func(x)";
+
+    ASSERT_EQ(lexer->get_token(stream), Lexer::tok_def);
+
+    ASSERT_EQ(lexer->get_token(stream), Lexer::tok_identifier);
+    ASSERT_EQ(lexer->get_identifier(), "my_func");
+
+    ASSERT_EQ(lexer->get_token(stream), 40);
+
+    ASSERT_EQ(lexer->get_token(stream), Lexer::tok_identifier);
+    ASSERT_EQ(lexer->get_identifier(), "x");
+
+    ASSERT_EQ(lexer->get_token(stream), 41);
 }
