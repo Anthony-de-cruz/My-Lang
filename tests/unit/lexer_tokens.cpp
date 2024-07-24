6f6077a1dcfb32ad8b2a603670a3088f321802ff
Anthony de Cruz <anthonydecruz@proton.me>
2024-07-24 14:36:28 +0100

    updated test

--- tests/unit/lexer_tokens.cpp
+++ tests/unit/lexer_tokens.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <gtest/gtest.h>
 #include <sstream>
 
@@ -6,18 +7,26 @@
 class LexerTest : public testing::Test {
   protected:
     Lexer *lexer;
+    std::stringstream *stream;
 
-    LexerTest() { lexer = new Lexer; };
+    LexerTest() {
+        lexer = new Lexer;
+        stream = new std::stringstream;
+    };
 };
 
-TEST_F(LexerTest, BasicTest) {
+TEST_F(LexerTest, StringOfTokens) {
 
-    auto stream = new std::stringstream;
+    *stream << "test string 123.3 #comment" << EOF;
 
-    *stream << "test string";
-
-    ASSERT_EQ(lexer->get_token(stream), -4);
+    ASSERT_EQ(lexer->get_token(stream), Lexer::tok_identifier);
     ASSERT_EQ(lexer->get_identifier(), "test");
-    ASSERT_EQ(lexer->get_token(stream), -4);
+
+    ASSERT_EQ(lexer->get_token(stream), Lexer::tok_identifier);
     ASSERT_EQ(lexer->get_identifier(), "string");
+
+    ASSERT_EQ(lexer->get_token(stream), Lexer::tok_number);
+    ASSERT_EQ(lexer->get_numeric(), 123.3);
+
+    ASSERT_EQ(lexer->get_token(stream), Lexer::tok_eof);
 }
