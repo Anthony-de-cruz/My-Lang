9fc1f4d2a47073dd3499a369bf5c063b25b738b3
Anthony de Cruz <anthonydecruz@proton.me>
2024-08-11 02:15:30 +0100

    added some quick tests for the parser

---
+++ tests/unit/parser.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <gtest/gtest.h>
+#include <sstream>
+#include <stdexcept>
+
+#include "parser.h"
+
+class ParserTest : public testing::Test {
+  protected:
+    Lexer *lexer;
+    std::stringstream *stream;
+    Parser *parser;
+
+    ParserTest() {
+        lexer = new Lexer;
+        stream = new std::stringstream;
+        parser = new Parser(lexer, stream);
+    };
+};
+
+TEST_F(ParserTest, BasicExpression) {
+    *stream << "x+y;";
+    try {
+        EXPECT_EQ(parser->handle_expression(), 59);
+        EXPECT_EQ(lexer->get_identifier(), "y");
+    } catch (std::runtime_error &exception) {
+        FAIL() << exception.what() << '\n';
+    }
+}
+
+TEST_F(ParserTest, BasicException) {
+    *stream << "x+;";
+    ASSERT_THROW(
+        {
+            try {
+                parser->handle_expression();
+            } catch (std::runtime_error &exception) {
+                EXPECT_STREQ("Syntax Error: Expected primary token, got ;",
+                             exception.what());
+                throw;
+            }
+        },
+        std::runtime_error);
+}
